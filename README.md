8c8a9943896a7a05522fc6152bb0e372b98a3c87
Billiam <billiamthesecond@gmail.com>
2025-04-25 17:20:28 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Fast Breeding
+
+VS mod to reduce (or increase) the speed of new animal husbandry generations.
+
---
+++ package.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+set -e
+
+rm ./vs-fast-breeding.zip
+cd src
+zip -r ../vs-fast-breeding.zip *
