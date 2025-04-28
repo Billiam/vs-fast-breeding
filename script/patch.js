cd5d53e9e7949dabb5dcc493601e40d6bd9fcdba
Billiam <billiamthesecond@gmail.com>
2025-04-28 14:15:04 -0500

    Fetch and update all mods from mods.json file

--- script/patch.js
+++ script/patch.js
@@ -4,6 +4,7 @@ import json5 from 'json5'
 import { glob } from 'glob'
 import { fileURLToPath } from 'url'
 import stringify from 'json-stable-stringify'
+import StreamZip from 'node-stream-zip'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
@@ -33,40 +34,43 @@ const buildPatch = (file, modId, pathSuffix, key, value) => {
   return patch
 }
 
-const buildModPatch = async (modId, files) => {
-  // order matters when adding config lib
+export const filePatch = async (modId, filename, fileData) => {
+  const patchPath = filename.match(/entities.*/)[0]
 
-  const results = (await Promise.all(files.map(async file => {
-    const patches = []
-    const patchPath = file.match(/entities.*/)[0]
+  return fileData.server.behaviors.reduce((patches, behavior, behaviorIndex) => {
+    const suffix = `${behaviorIndex}/`
 
-    const data = json5.parse(await fs.readFile(file, 'utf8'))
+    Object.entries(behavior).forEach(([key, value]) => {
+      const keyWithoutType = key.replace(/ByType$/, '')
 
-    data.server.behaviors.forEach((behavior, behaviorIndex) => {
-      const suffix = `${behaviorIndex}/`
-      Object.entries(behavior).forEach(([key, value]) => {
-        const keyWithoutType = key.replace(/ByType$/, '')
+      if (BEHAVIOR_TARGETS.includes(key)) {
+        patches.push(buildPatch(patchPath, modId, suffix, key, value))
 
-        if (BEHAVIOR_TARGETS.includes(key)) {
-          patches.push(buildPatch(patchPath, modId, suffix, key, value))
-
-        } else if (BEHAVIOR_TARGETS.includes(keyWithoutType)) {
-          Object.entries(value).forEach(([subKey, value]) => {
-            patches.push(buildPatch(patchPath, modId, `${suffix}${key}/`, subKey, value))
-          })
-        }
-      })
+      } else if (BEHAVIOR_TARGETS.includes(keyWithoutType)) {
+        Object.entries(value).forEach(([subKey, value]) => {
+          patches.push(buildPatch(patchPath, modId, `${suffix}${key}/`, subKey, value))
+        })
+      }
     })
 
     return patches
-  }))).flat()
+  }, [])
+}
 
-  const output = {
-    configLib: {}
+const buildModPatch = async (modId, files) => {
+  // order matters when adding config lib
+  const results = []
+  for await (const { file, data } of files) {
+    results.push(...await filePatch(modId, file, data))
   }
   results.sort((a, b) => {
     return a.file.localeCompare(b.file) || a.sortKey.localeCompare(b.sortKey)
   })
+
+  const output = {
+    configLib: {}
+  }
+
   output.patches = results
 
   output.patches.forEach((patch, patchIndex) => {
@@ -88,10 +92,91 @@ const sortedKeys = (a, b) => {
   }
   return a.key.localeCompare(b.key)
 }
-export default async modPath => {
-  const modInfoPath = path.join(modPath, 'modinfo.json')
-  const modId = json5.parse(await fs.readFile(modInfoPath, 'utf8')).modid
 
+class DirectoryModReader {
+  constructor (path) {
+    this.path = path
+  }
+
+  get modId () {
+    if (!this._modPromise) {
+      this._modPromise = new Promise(async resolve => {
+        const fileData = await fs.readFile(path.join(this.path, 'modinfo.json'), 'utf8')
+        resolve(json5.parse(fileData).modid)
+      })
+    }
+    return this._modPromise
+  }
+
+  async * files () {
+    const modId = await this.modId
+    const jsonGlob = path.join(this.path, `assets/${modId}/entities/**/*.json`)
+    const fileList = await glob(jsonGlob, {})
+    for (const file of fileList) {
+      yield {
+        file,
+        data: json5.parse(await fs.readFile(file, 'utf8'))
+      }
+    }
+  }
+
+  async cleanup () {}
+}
+
+class ZipModReader {
+  constructor (path) {
+    this.path = path
+  }
+
+  get zipFile () {
+    if (!this.zip) {
+      this.zip = new StreamZip.async({ file: this.path })
+    }
+    return this.zip
+  }
+
+  async cleanup () {
+    if (this.zip) {
+      return this.zip.close()
+    }
+  }
+
+  get modId () {
+    if (!this._modPromise) {
+      this._modPromise = new Promise(async resolve => {
+        const buffer = await this.zipFile.entryData('modinfo.json')
+        const manifest = buffer.toString()
+        resolve(json5.parse(manifest).modid)
+      })
+    }
+    return this._modPromise
+  }
+
+  async * files () {
+    const modId = await this.modId
+    const zipFile = await this.zipFile
+
+    const entries = await zipFile.entries()
+
+    const fileList = Object.keys(entries).filter(filename => {
+      return filename.startsWith(`assets/${modId}/entities/`) && filename.endsWith('.json')
+    })
+
+    for (const file of fileList) {
+      const entry = await zipFile.entryData(file)
+      yield {
+        file,
+        data: json5.parse(entry.toString())
+      }
+    }
+  }
+}
+
+export default async modPath => {
+  const readerType = modPath.endsWith('.zip') ? ZipModReader : DirectoryModReader
+  const reader = new readerType(modPath)
+
+  const modId = await reader.modId
   if (!modId) {
     throw new Error('mod could not be identified')
   }
@@ -101,15 +186,13 @@ export default async modPath => {
     modDirectory.startsWith(str)
   )
 
-  const jsonGlob = path.join(modPath, `assets/${modId}/entities/**/*.json`)
+  const patchData = await buildModPatch(modId, reader.files())
+  await reader.cleanup()
 
-  const fileList = await glob(jsonGlob, {})
-
-  const patchData = await buildModPatch(modId, fileList)
   const patchProjectPath = `patches/compatibility/${prefix ? prefix + '/' : ''}${modId}.json`
   const patchPath = path.resolve(__dirname, `../src/assets/fastbreeding/${patchProjectPath}`)
 
-  await fs.writeFile(patchPath, stringify(patchData.patches, { cmp: sortedKeys, space: '  ' }))
+  await fs.writeFile(patchPath, stringify(patchData.patches, { cmp: sortedKeys, space: '  ' }) + '\n')
 
   const configLibFile = path.resolve(__dirname, '../src/assets/fastbreeding/config/configlib-patches.json')
   const configLibData = JSON.parse(await fs.readFile(configLibFile, 'utf8'))
@@ -117,5 +200,5 @@ export default async modPath => {
   const outputKey = `fastbreeding:${patchProjectPath}`
   configLibData.patches.integer[outputKey] = patchData.configLib
 
-  return fs.writeFile(configLibFile, stringify(configLibData, { cmp: sortedKeys, space: '  ' }))
+  return fs.writeFile(configLibFile, stringify(configLibData, { cmp: sortedKeys, space: '  ' }) + '\n')
 }
---
+++ script/update-all.js
@@ -0,0 +1,68 @@
+#!/usr/bin/env node
+import { fileURLToPath } from 'url'
+import path from 'path'
+import { promises as fs, existsSync, createWriteStream } from 'fs'
+import semver from 'semver'
+import patch from './patch.js'
+import stringify from 'json-stable-stringify'
+
+import { Readable } from 'node:stream'
+import { finished } from 'stream/promises'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const downloadZip = async (url, filename) => {
+  console.log('Fetching', url)
+  const tmpDirPath = path.resolve(__dirname, '../tmp')
+
+  const destination = path.join(tmpDirPath, filename)
+  if (existsSync(destination)) {
+    return destination
+  }
+
+  if (!existsSync(tmpDirPath)) {
+    await fs.mkdir(tmpDirPath)
+  }
+  
+  const res = await fetch(url, { redirect: 'follow' })
+
+  const fileStream = createWriteStream(destination, { flags: 'w' })
+  await finished(Readable.fromWeb(res.body).pipe(fileStream))
+
+  return destination
+}
+
+const updateMod = async (modId, lastVersion) => {
+  console.log('Updating', modId)
+  const response = await fetch(`https://mods.vintagestory.at/api/mod/${modId}`, { redirect: 'follow' })
+  const data = await response.json()
+
+  if (data.statuscode === '200') {
+    const latestRelease = data.mod.releases[0]
+    const releaseNewer = !lastVersion || semver.gt(latestRelease.modversion, lastVersion)
+
+    if (releaseNewer) {
+      console.log('Fetching newer release', `${latestRelease.modversion} > ${lastVersion}`)
+      const zipPath = await downloadZip(latestRelease.mainfile, latestRelease.filename)
+      await patch(zipPath)
+
+      return latestRelease.modversion
+    }
+  }
+}
+
+(async () => {
+  const modsConfigPath = path.join(__dirname, '../mods.json')
+  const modVersions = JSON.parse(await fs.readFile(modsConfigPath, 'utf8'))
+
+  const modIds = Object.keys(modVersions.mods)
+  for (const modId of modIds) {
+    //TODO: limit updates to targeted VS versions
+    const newVersion = await updateMod(modId, modVersions.mods[modId])
+    if (newVersion) {
+      modVersions.mods[modId] = newVersion
+    }
+  }
+  return fs.writeFile(modsConfigPath, stringify(modVersions, { space: '  ' }) + '\n')
+})()
