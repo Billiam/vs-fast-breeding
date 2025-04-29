7d3176856da462935b5663dba4a8571cc97f8a3a
Billiam <billiamthesecond@gmail.com>
2025-04-29 15:15:49 -0500

    Separate build from mod version

--- script/package.sh
+++ script/package.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
-NODE_VERSION=$(node -p -e "require('./package.json').version")
+NODE_VERSION=$(node -p -e "require('./src/modinfo.json').version")
 
 rm -f ./fast-breeding*.zip
 cd src
