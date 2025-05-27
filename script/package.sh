9a404a85dfff0bdc57d95ab0c1f5f6eedbbc8505
Billiam <billiamthesecond@gmail.com>
2025-05-27 11:24:38 -0500

    Add zip message

--- script/package.sh
+++ script/package.sh
@@ -5,3 +5,4 @@ NODE_VERSION=$(node -p -e "require('./src/modinfo.json').version")
 rm -f ./fast-breeding*.zip
 cd src
 zip -r ../fast-breeding_${NODE_VERSION}.zip *
+echo "Built fast-breeding_${NODE_VERSION}.zip"
